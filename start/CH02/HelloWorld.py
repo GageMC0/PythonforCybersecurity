da26ff5e71e3d3c23361387062492c65667b106a
Gage <coonradt.gage@student.greenriver.edu>
2025-04-12 21:24:11 -0700

    Added dessert

--- start/CH02/HelloWorld.py
+++ start/CH02/HelloWorld.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python
 # Created 
+
+#Promt user for name
+user_name = input ("What is your name? ")
+user_name = "Gage"
+#Print hello and user's name
+print ("Hello {0}".format (user_name))
+
+print(f"Hello {user_name}")
+
+print ("Hello " + user_name)
+
+print ("Hello again", user_name)

282b8ef0b05546ab069e3ac6d4990c1db363fe5f
Gage <coonradt.gage@student.greenriver.edu>
2025-04-20 19:09:05 -0700

    IT102 Conditionals

--- start/CH03/Conditionals.py
+++ start/CH03/Conditionals.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 # example workign with conditionals
 #By 
+
+answer = input("Is today a good day? (y/n)")
+if answer == ("y"):
+    print("Yes it is!")
+else:
+    print("Sorry to hear that")
